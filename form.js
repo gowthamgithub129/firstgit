5bf1eec4f5f30390f7db2a7e557e812e9ad06651
gowthamreddy129 <gowthamreddy4179@gmail.com>
2023-07-17 21:24:42 +0530

    chat app

--- form.js
+++ form.js
@@ -4,28 +4,47 @@ const fs = require('fs');
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
-    // Send the HTML form to the client
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`
-      <form method="POST" action="/submit" enctype="multipart/form-data">
-        <input type="text" name="username" placeholder="Username" /><br />
-        <input type="file" name="avatar" /><br />
-        <button type="submit">Submit</button>
-      </form>
-    `);
+    // Read existing messages from the file
+    fs.readFile('messages.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        data = ''; // If an error occurs, set the data to an empty string
+      }
+
+      // Send the HTML form with existing messages to the client
+      res.setHeader('Content-Type', 'text/html');
+      res.end(`
+        <h2>Existing Messages:</h2>
+        <div>${data}</div>
+        <hr />
+        <form method="POST" action="/submit" enctype="multipart/form-data">
+          <input type="text" name="message" placeholder="Enter a message" /><br />
+          <button type="submit">Submit</button>
+        </form>
+      `);
+    });
   } else if (req.method === 'POST' && req.url === '/submit') {
     // Handle the form submission
-    const writeStream = fs.createWriteStream('output.txt');
+    let body = '';
 
-    req.pipe(writeStream)
-      .on('finish', () => {
+    req.on('data', chunk => {
+      body += chunk;
+    });
+
+    req.on('end', () => {
+      const message = body.split('=')[1];
+
+      // Append the new message to the file
+      fs.appendFile('messages.txt', `${message}\n`, 'utf8', err => {
+        if (err) {
+          console.error(err);
+        }
+
         // Redirect with a 302 response
-        res.writeHead(302, { Location: '/success' });
+        res.writeHead(302, { Location: '/' });
         res.end();
       });
-  } else if (req.url === '/success') {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Form submitted successfully!');
+    });
   } else {
     // Return a 404 response for unknown routes
     res.statusCode = 404;
