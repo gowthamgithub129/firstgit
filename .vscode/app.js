08cdbded4a1b308051f50a62fd1bbc241133ffd6
gowthamreddy129 <gowthamreddy4179@gmail.com>
2023-07-18 22:10:07 +0530

    product page

--- .vscode/app.js
+++ .vscode/app.js
@@ -1,15 +1,30 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// GET route to display the form
+app.get('/add-product', (req, res) => {
+  res.send(`
+    <form action="/add-product" method="POST">
+      <input type="text" name="productName" placeholder="Product Name">
+      <input type="text" name="productSize" placeholder="Product Size">
+      <button type="submit">Submit</button>
+    </form>
+  `);
 });
-const app.use((req, res, next) => {
-    console.log('In another middleware!');
+
+// POST route to handle form submission
+app.post('/add-product', (req, res) => {
+  const productName = req.body.productName;
+  const productSize = req.body.productSize;
+  console.log(`Product Name: ${productName}`);
+  console.log(`Product Size: ${productSize}`);
+  res.send('Product added successfully!');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
